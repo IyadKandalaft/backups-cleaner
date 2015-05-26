24b938bc0fe26c324af01c77d7b2a009d672a122
Iyad Kandalaft <kandalafti@agr.gc.ca>
2015-05-25 22:33:11 -0400

    Implemented monthly and yearly retention methods.

--- backupscleaner/cleaner.py
+++ backupscleaner/cleaner.py
@@ -5,6 +5,7 @@ Created on 2015-05-22
 '''
 
 import backupscleaner.builder
+import datetime
 from hamster.widgets.timechart import DAY
 
 class BackupsCleaner(object):
@@ -12,7 +13,7 @@ class BackupsCleaner(object):
     classdocs
     '''
 
-    def __init__(self, backups_file_list, daily=14, weekly=6, monthly=4, annually=2):
+    def __init__(self, backups_file_list, daily=14, weekly=6, monthly=6, annually=3):
         '''
         Constructor
         '''
@@ -21,7 +22,7 @@ class BackupsCleaner(object):
                             'daily': daily,
                             'weekly': weekly,
                             'monthly': monthly,
-                            'annualy': annually
+                            'annually': annually
                          }
         
     def clean(self, dry_run=False):
@@ -35,19 +36,50 @@ class BackupsCleaner(object):
                     'annually': 0
                     }
         
+        
+        #retain_days_date = today - datetime.timedelta(days=14)
+        
         for bucket in file_list.values():
             for year in reversed(sorted(bucket.keys())):
                 for month in reversed(sorted(bucket[year].keys())):
                     for day in reversed(sorted(bucket[year][month].keys())):
                         print year, month, day
-                        if retained['daily'] < schedule['daily']:
-                            print "retain"
-                            retained['daily'] += 1
+                            #backup_date = datetime.date(year, month, day)
+                            #if backup_date > retain_days_date:
+                            #elif month > today.month - 2:
+                                
+                            #bucket[year][month][day]['retain'] = True
+                            
+                            #retained['daily'] += 1
+        self.__flag_monthly(file_list, schedule)
+        self.__flag_annually(file_list, schedule)
     
     #def __flag_daily(self):
         
     #def __flag_weekly(self):
         
-    #def __flag_monthly(self):
-        
-    #def __flag_annually(self):
+    def __flag_monthly(self, file_list, schedule):
+        today = datetime.date.today()
+        for multiplier in range(1, schedule['monthly']):
+            ret_date = (today - datetime.timedelta(days=30*multiplier))
+            for bucket in file_list.keys():
+                if file_list[bucket].has_key(ret_date.year) and file_list[bucket][ret_date.year].has_key(ret_date.month):
+                    oldest_day = reversed(sorted(file_list[bucket][ret_date.year][ret_date.month].keys())).pop()
+                    file_list[bucket][ret_date.year][ret_date.month][oldest_day]['retain'] = True
+                    print ret_date.year, ret_date.month, oldest_day
+                else:
+                    print 'No monthly backup for ', bucket, ret_date.year, ret_date.month
+                
+            #print month 
+    def __flag_annually(self, file_list, schedule):
+        today = datetime.date.today()
+        for multiplier in range(1, schedule['annually']):
+            ret_date = (today - datetime.timedelta(days=365*multiplier))
+            for bucket in file_list.values():
+                if bucket.has_key(ret_date.year):
+                    oldest_month = reversed(sorted(bucket.keys())).pop()
+                    oldest_day = reversed(sorted(bucket[oldest_month].kceys())).pop()
+                    bucket[ret_date.year][oldest_month][oldest_day]['retain'] = True
+                    print ret_date.year, oldest_month, oldest_day
+                else:
+                    print 'No yearly backup for ', bucket, ret_date.year
